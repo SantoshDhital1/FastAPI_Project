662ea320e7742ceb55c711a3427d961f7bd34751
Santosh <dhitalsantoshgsd999@gmail.com>
2025-12-05 18:21:15 +0545

    added ML integration

---
+++ app/cache/redis_cache.py
@@ -0,0 +1,14 @@
+import redis
+import json
+from core.config import settings
+
+redis_client = redis.Redis.from_url(settings.REDIS_URL)
+
+def get_cached_predictions(key: str):
+    value = redis_client.get(key)
+    if value:
+        return json.loads(value)
+    return None
+
+def set_cached_prediction(key:str, value: dict, expire: int = 3600):
+    redis_client.setex(key, expire, json.dumps(value))
